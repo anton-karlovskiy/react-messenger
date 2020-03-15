6e9677a1fb9da73393258d6e24dac180a226162c
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-03-16 01:16:16 +0300

    chore: cleaned up Message

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,36 +1,36 @@
+
 import React from 'react';
 import moment from 'moment';
 import './Message.css';
 
-export default function Message(props) {
-    const {
-      data,
-      isMine,
-      startsSequence,
-      endsSequence,
-      showTimestamp
-    } = props;
+const Message = ({
+  data,
+  isMine,
+  startsSequence,
+  endsSequence,
+  showTimestamp
+}) => {
+  const friendlyTimestamp = moment(data.timestamp).format('LLLL');
 
-    const friendlyTimestamp = moment(data.timestamp).format('LLLL');
-    return (
-      <div className={[
-        'message',
-        `${isMine ? 'mine' : ''}`,
-        `${startsSequence ? 'start' : ''}`,
-        `${endsSequence ? 'end' : ''}`
-      ].join(' ')}>
-        {
-          showTimestamp &&
-            <div className="timestamp">
-              { friendlyTimestamp }
-            </div>
-        }
-
-        <div className="bubble-container">
-          <div className="bubble" title={friendlyTimestamp}>
-            { data.message }
-          </div>
+  return (
+    <div className={[
+      'message',
+      `${isMine ? 'mine' : ''}`,
+      `${startsSequence ? 'start' : ''}`,
+      `${endsSequence ? 'end' : ''}`
+    ].join(' ')}>
+      {showTimestamp && (
+        <div className='timestamp'>
+          {friendlyTimestamp}
+        </div>
+      )}
+      <div className='bubble-container'>
+        <div className='bubble' title={friendlyTimestamp}>
+          {data.message}
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
+
+export default Message;
